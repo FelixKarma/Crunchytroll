17f098e404f0814808fe283a4186f6bc019a249f
JorysB1 <jojorys2@gmail.com>
2023-06-01 14:08:28 +0200

    ajout popup catégories et nous contacter

---
+++ js/buttoncategorie.js
@@ -0,0 +1,10 @@
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "block";
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+    document.getElementById("popup").style.display = "none";
+}
+
---
+++ js/buttoncontact.js
@@ -0,0 +1,9 @@
+function openbuttoncontact() {
+    document.getElementById("buttoncontactOverlay").style.display = "block";
+    document.getElementById("buttoncontact").style.display = "block";
+}
+
+function closebuttoncontact() {
+    document.getElementById("buttoncontactOverlay").style.display = "none";
+    document.getElementById("buttoncontact").style.display = "none";
+}
